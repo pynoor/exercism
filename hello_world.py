059db46faad3d4ec977dba547d62cfa5803963e9
pynoor <32771762+pynoor@users.noreply.github.com>
2017-10-20 19:23:46 +0200

    Create function
    This transforms the initial code into a function.

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,4 @@
+def hello( str ):
     "This function returns the string \"Hello, World!\""
     print ("Hello, World!")
     return
-
--- leap.py
+++ leap.py
@@ -1,13 +1,15 @@
-    year = int(input("What year is it? : "))
+def leap (year):
     if year % 100 == 0 or year % 400 == 0:
              print (str(year) + " is not a leap year.")
-             continue
     elif year % 4 == 0 :
              print (str(year) + " is  a leap year.")  
-             continue
     else :
         print (str(year) + " is not a leap year.")
-        continue 
+    return
+
+leap (1992)
+
+
 
 
 
