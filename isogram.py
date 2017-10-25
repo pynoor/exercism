eeb33e559b3937a05618995bcaba8e7eea31160f
pynoor <32771762+pynoor@users.noreply.github.com>
2017-10-25 14:59:18 +0200

    Fix lower-/upper- case problem
    This change converts the whole word in the argument to upper case to eliminate the issue of upper and lower cases. This function still does not know what to do with empty spaces.

--- isogram.py
+++ isogram.py
@@ -1,15 +1,21 @@
 def is_isogram(word = " "):
     x = 0
-    for letter in word :
-        if word.count(str(letter)) == 1:
+    word_2 = str.upper(word)
+    for letter in word_2 :
+        if word_2.count(letter) == 1 and letter != " " :
             x = x+1
-            if len(word) == x :
+            if len(word_2) == x :
                 print(word + " is an isogram.")
                 return True
-            
+        elif letter == " " :
+            x = x
+            if len(word_2) == x :
+                return True
+            else :
+                continue
         else :
             print(word + " is not an isogram.")
             return False
 
-# takes into consideration that capital and small letters are different
-            
+
+# Interpretes spaces as letters, hence doesn't know what to do with them   
