4657d0e5ec51d69fcc53ea954182b2e251a64c84
pynoor <32771762+pynoor@users.noreply.github.com>
2017-10-30 10:28:02 +0100

    Add hamming exercise

---
+++ hamming.py
@@ -0,0 +1,12 @@
+def distance(strand_a, strand_b):
+    count = 0
+    if len(strand_a) == len(strand_b) :
+            for x in range(len(strand_a)) :
+                if strand_a[x] != strand_b[x] :
+                    print (strand_a[x])
+                    count = count + 1
+                else :
+                    count = count
+            return (count)
+    else :
+        return (ValueError)
