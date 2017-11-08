71b2e3eeeb34a3e4d572b86d96f9ead987e30968
pynoor <32771762+pynoor@users.noreply.github.com>
2017-11-08 10:48:10 +0100

    Fix empty string issue
    In this fix, the function is not giving out empty strings as words anymore.

--- word_count.py
+++ word_count.py
@@ -1,3 +1,4 @@
+
 import re
 def word_count(phrase):
     words = phrase.split()
@@ -5,8 +6,10 @@ def word_count(phrase):
     new = []
     for x in words :
         regex = re.compile('[^a-zA-Z]')
-        regex = regex.sub('', str(x))    
+        regex = regex.sub('', str(x)) 
         frequencies.append(words.count(x))
-        new.append(str(regex) + " : " + str(frequencies[words.index(x)]))
+        if str(regex) == "" :
+            continue
+        new.append("'" + str(regex) + "'" + " : " + str(frequencies[words.index(x)]))
 
-    print(new)
+    return (new)
