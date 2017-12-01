17a52a6976071e944eb11a94c238025d829015e5
pynoor <32771762+pynoor@users.noreply.github.com>
2017-12-01 13:45:08 +0100

    Refactor Codes

--- isogram.py
+++ isogram.py
@@ -1,21 +1,16 @@
 def is_isogram(word = " "):
-    x = 0
-    word_2 = str.upper(word)
-    for letter in word_2 :
-        if word_2.count(letter) == 1 and letter != " " :
-            x = x+1
-            if len(word_2) == x :
-                print(word + " is an isogram.")
-                return True
-        elif letter == " " :
-            x = x
-            if len(word_2) == x :
-                return True
-            else :
+    import string
+    alpha = string.ascii_lowercase
+    word = ("").join(word.split(" "))
+    word = str.lower(word)
+    if word == "":
+        return True
+    else:
+        for letter in word :
+            if letter not in alpha:
                 continue
-        else :
-            print(word + " is not an isogram.")
-            return False
-
-
-# Interpretes spaces as letters, hence doesn't know what to do with them   
+            elif word.count(letter) == 1:
+                continue
+            else :
+                return False 
+        return True
--- rna_transcription.py
+++ rna_transcription.py
@@ -1,20 +1,10 @@
 def to_rna(dna_strand):
-    for letter in dna_strand :  
-        if letter == "G" :
-            x = ["C", dna_strand.index(letter)]
-        elif letter == "C" :
-            y = ["G", dna_strand.index(letter)]
-        elif letter == "T" :
-            z = ["A", dna_strand.index(letter)]
-        elif letter == "A" :
-            v = ["U", dna_strand.index(letter)]
-    def takeSecond(elem) :
-        return elem[1]
-    total = [x, y, z, v]
-    total.sort(key = takeSecond)
-    output = ""
-    for s in total :
-        output = output + str(x[0])
-    return (output)
-
-# Does not work for input that does not consist of all the letters G, C, T, A.
+    RNA = {"G":"C", "C":"G", "T":"A", "A":"U"}
+    dna_list = list(dna_strand)
+    rna_list = []
+    for x in dna_list:
+        if x not in RNA:
+            return ""
+        rna_list.append(RNA[x])
+    return "".join(rna_list)
+        
